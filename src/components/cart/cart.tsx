8c2337273cb9bcdd680f5e81e6e2b40c11e9cc33
Alex <ajoshua14@icloud.com>
2023-11-29 17:52:19 -0800

    Begin implementing layout for medium screen sizes

--- src/components/cart/cart.tsx
+++ src/components/cart/cart.tsx
@@ -54,8 +54,8 @@ const Cart: FC<CartProps> = ({ className, ...props }) => {
   }
 
   return (
-    <div className={cn("z-30 fixed top-20 left-1/2 -translate-x-1/2 h-[270px] w-full max-w-400px p-2", className)} {...props}>
-      <div className="w-full h-full flex flex-col bg-greyblue-300 rounded-xl">
+    <div className={cn("z-30 fixed top-20 right-1/2 translate-x-1/2 sm:right-0 sm:translate-x-0 h-[270px] w-full max-w-[400px] p-2", className)} {...props}>
+      <div className="w-full h-full flex flex-col bg-greyblue-300 rounded-xl shadow-xl">
         <div className="h-1/4 w-full px-6 flex items-center border-b-[1px] border-b-greyblue-500/50 text-lg font-semibold tracking-wide">
           Cart
         </div>
--- src/components/navigation/navBar.tsx
+++ src/components/navigation/navBar.tsx
@@ -10,22 +10,24 @@ interface NavBarProps {
 
 const NavBar: FC<NavBarProps> = ({ }) => {
   return (
-    <div className="z-40 fixed top-0 h-16 w-full flex items-center justify-between px-6 bg-white/40 backdrop-blur-xl">
-      <div className="h-full flex items-center justify-center gap-4 text-xs">
-        <MobileMenu />
-        <div className="text-2xl font-semibold tracking-tight px-1 subpixel-antialiasedased">
-          {`novaglide`.toLowerCase()}
+    <div className="z-40 fixed top-0 nav-height w-full page-gutter-x bg-white/40 backdrop-blur-xl">
+      <div className="w-full h-full flex items-center justify-between sm:border-b-[1px] border-b-greyblue-500/40">
+        <div className="h-full flex items-center justify-center gap-4 text-xs">
+          <MobileMenu />
+          <div className="text-2xl font-semibold tracking-tight px-1 subpixel-antialiasedased">
+            {`novaglide`.toLowerCase()}
+          </div>
         </div>
-      </div>
-      <div className="flex items-center justify-center gap-6">
-        <div>
-          <CartButton />
-        </div>
-        <div>
-          <Avatar>
-            <AvatarImage src={`/images/image-avatar.png`} alt="profile image" width={32} height={32} />
-            <AvatarFallback>AL</AvatarFallback>
-          </Avatar>
+        <div className="flex items-center justify-center gap-6">
+          <div>
+            <CartButton />
+          </div>
+          <button>
+            <Avatar className="border-[1px] border-transparent hover:border-orange">
+              <AvatarImage src={`/images/image-avatar.png`} alt="profile image" width={32} height={32} />
+              <AvatarFallback>AL</AvatarFallback>
+            </Avatar>
+          </button>
         </div>
       </div>
     </div>
