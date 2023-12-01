381927d7164b670a4f508a67105f2772f1392b78
Alex <ajoshua14@icloud.com>
2023-11-30 18:48:26 -0800

    Outline ui components to modularize

---
+++ src/components/button.tsx
@@ -0,0 +1,2 @@
+// Create modular button for add to cart and checkout button
+// note that we should forward refs to the button component for animation
---
+++ src/components/image.tsx
@@ -0,0 +1,3 @@
+// Create modular image for product images including main image and carousel
+// note that images should be able to have an onClick callback passed in
+// also note that we should forward refs to the image component
