7f1af4cad2eae47eea9b98afdda169c68e88fe86
Alex <ajoshua14@icloud.com>
2023-12-01 19:41:24 -0800

    Begin modularizing button component

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,2 +1,28 @@
 // Create modular button for add to cart and checkout button
 // note that we should forward refs to the button component for animation
+import { cn } from '@/lib/utils'
+import { FC, HTMLAttributes } from 'react'
+
+interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
+  text: string
+  Icon?: React.ReactNode
+}
+
+const Button: FC<ButtonProps> = ({ text, Icon, onClick, className, ...props }) => {
+  return (
+    <button
+      onClick={onClick}
+      className={cn(`h-full w-full flex items-center justify-center gap-4 
+                  bg-orange rounded-xl shadow-xl shadow-orange/30
+                   font-semibold text-greyblue-300`, className)}
+      {...props}
+    >
+      {Icon}
+      <p>
+        {text}
+      </p>
+    </button>
+  )
+}
+
+export default Button
--- src/components/cart/addToCart.tsx
+++ src/components/cart/addToCart.tsx
@@ -4,10 +4,24 @@ import { FC, use, useState } from 'react'
 import Image from 'next/image'
 import { useDispatch, useSelector } from 'react-redux'
 import { addItem, removeItem } from '@/lib/redux/reducers/cartSlice'
+import Button from '../button'
 
 interface AddToCartProps {
 
 }
+
+const CartIcon = (
+  <svg
+    width="28"
+    height="20"
+    xmlns="http://www.w3.org/2000/svg"
+    className='fill-greyblue-300'
+  >
+    <path
+      d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z"
+    />
+  </svg>
+)
 
 const AddToCart: FC<AddToCartProps> = ({ }) => {
   const { count } = useSelector((state: any) => state.cart)
@@ -46,8 +60,10 @@ const AddToCart: FC<AddToCartProps> = ({ }) => {
         </button>
 
       </div>
-
-      <button
+      <div className="h-1/2 w-full flex">
+        <Button text={`Add to cart`} Icon={CartIcon} onClick={addToCart} />
+      </div>
+      {/* <button
         onClick={addToCart}
         className="h-1/2 w-full flex items-center justify-center gap-4 
                   bg-orange rounded-xl shadow-xl shadow-orange/30
@@ -65,7 +81,7 @@ const AddToCart: FC<AddToCartProps> = ({ }) => {
         <p>
           {`Add to cart`}
         </p>
-      </button>
+      </button> */}
     </div>
   )
 }
