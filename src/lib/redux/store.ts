5b88f7706b0d1b31b1b5fc2e086be724827eac91
Alexander Joshua <59298565+Alexjoshua14@users.noreply.github.com>
2023-12-09 19:22:29 +0000

    Update store.ts

--- src/lib/redux/store.ts
+++ src/lib/redux/store.ts
@@ -2,6 +2,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import cartReducer from './reducers/cartSlice'
 
+/**
+ * Store contains just one simple reducer for
+ * storing demo state of cart items. 
+ */
 export default configureStore({
   reducer: {
     cart: cartReducer,
